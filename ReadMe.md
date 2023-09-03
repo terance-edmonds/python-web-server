6c05ea910b934c4d77f970aa41757961293ace82
Terance Edmonds <teranceedmonds20@gmail.com>
2023-09-03 18:46:24 +0530

    sample files and thread use

--- ReadMe.md
+++ ReadMe.md
@@ -1,5 +1,35 @@
 # Simple Python Web Server (Socket Programming)
 
+This is a simple web server developed with python sockets. Server is compatible of serving various file types and also supports serving multiple clients at once.
+
+## Example links
+
+| Type      | Route                                                               |
+| --------- | ------------------------------------------------------------------- |
+| **.php**  | [/ or /index.php](http://localhost:2728)                            |
+| **.html** | [/files/sample.html](http://localhost:2728/files/sample.html)       |
+| **.css**  | [/style.css](http://localhost:2728/style.css)                       |
+| **.js**   | [/script.js](http://localhost:2728/script.js)                       |
+| **.jpg**  | [/images/parallax1.jpg](http://localhost:2728/images/parallax1.jpg) |
+| **.json** | [/files/sample.json](http://localhost:2728/files/sample.json)       |
+| **.xml**  | [/files/sample.xml](http://localhost:2728/files/sample.xml)         |
+| **.txt**  | [/files/sample.txt](http://localhost:2728/files/sample.txt)         |
+| **.pdf**  | [/files/sample.pdf](http://localhost:2728/files/sample.pdf)         |
+| **.xlsx** | [/files/sample.xlsx](http://localhost:2728/files/sample.xlsx)       |
+
 ## Requirements
 
-PHP installed
+- PHP 8.2.4 or up ( `php -v` for version)
+- Python3 ( `python -v` for version)
+
+## Usage
+
+Create a folder named as `htdocs` in the root directory, and add the web files to it.
+
+Than, to start the server, Run the below code.
+
+```bash
+python serve.py
+```
+
+Now open the browser and head over to [localhost:2728](http://localhost:2728).
--- serve.py
+++ serve.py
@@ -3,7 +3,9 @@ import sys
 import glob
 import subprocess
 import json
-import time
+import urllib.parse
+import os
+from threading import Thread
 
 # server configs
 HOST='localhost'
@@ -32,19 +34,27 @@ def formatRequest(client_socket):
     # get the first line and split by space
     data = lines[0].split()
 
-    return [data[0], data[1]]
+    method = data[0].upper()
+    # parse the url
+    route = urllib.parse.unquote_plus(data[1])
 
-def formatPath(path):
+    return [method, route]
+
+def formatPath(route):
     # if the path has no file set it to index file
-    if path == '/':
-        path = '/index.*'
+    if route[-1] == '/':
+        route = '/index.*'
     
     # set to relative file path
-    path = f"{FILE_DIR}{path}"
+    route = f"{FILE_DIR}{route}"
 
-    return path
+    # if this is a directory search for a index file
+    if(os.path.isdir(route)):
+        route = f'{route}/index.*'
 
-def formatResponse(method = "GET", path = "/", content_type = "text/plain", status_code = 200, status_message = "OK", data = "", document = False):
+    return route
+
+def formatResponse(client_address, method = "GET", route = "/", content_type = "text/plain", status_code = 200, status_message = "OK", data = "", document = False):
     # set status
     header = f"HTTP/1.1 {status_code} {status_message}\r\n"
     # set response content type
@@ -60,21 +70,21 @@ def formatResponse(method = "GET", path = "/", content_type = "text/plain", stat
         data = data.encode('utf-8')
 
     # print on terminal
-    log(method=method, content_type='text/plain', status_code=status_code, path=path)
+    log(client_address=client_address, method=method, content_type='text/plain', status_code=status_code, route=route)
     
     # return the encoded headers
     return [header, data]
 
-def getExtension(path):
+def getExtension(route):
     # split the path by '.'
-    strings = path.split('.')
+    strings = route.split('.')
 
     # get the last string from the list
     return strings[-1]
 
-def log(method, content_type, status_code, path):
+def log(client_address, method, content_type, status_code, route):
     # format path
-    path = path.replace('\\', '/')
+    route = route.replace('\\', '/')
 
     # create string with colors
     if(status_code == 404):
@@ -84,11 +94,78 @@ def log(method, content_type, status_code, path):
         method = f"\33[32m[{method}]\033[0m" + "\t"
         status_code = f"\33[104m {status_code} \033[0m" + "\t"
 
+    client_address = f"\33[33m{client_address}\033[0m" + "\t"
     content_type = f"\33[90m{content_type}\033[0m" + "\t"
-    path = f"{path}" + "\t"
+    route = f"{route}" + "\t"
 
     # print the content
-    print("{0:<8} {1:<8} {2:<8} {3:<20}".format(method, content_type, status_code, path))
+    print("{0:<8} {1:<8} {2:<15} {3:<8} {4:<20}".format(client_address, method, content_type, status_code, route))
+
+def on_client(client_socket, client_address):
+    while True:
+        [method, route] = formatRequest(client_socket)
+
+        # if method or path is not found continue to the next
+        if not method or not route:
+            break
+        
+        # if the method is get::proceed
+        if method == 'GET':
+            route = formatPath(route)
+
+            if len(glob.glob(route)) > 0:
+                # set the found file path
+                route = glob.glob(route)[0]
+                # extract the file extension
+                extension = getExtension(route)
+                # get the content type and execution type
+                content_type, execution_type, is_document = content_types.get(extension, ["text/plain", None, False])
+                
+                if execution_type:
+                    # execute the file (eg: php) and get the output
+                    output = subprocess.check_output([execution_type, route], shell=True, universal_newlines=True)
+                elif is_document:
+                    # read the file as binary
+                    with open(route, 'rb') as file:
+                        output = file.read()
+                else:
+                    # read the file as text
+                    with open(route, 'r') as text:
+                        output = text.read()
+            
+                # format the response with data
+                [header, data] = formatResponse(
+                    client_address=client_address,
+                    method=method,
+                    route=route,
+                    content_type=content_type,
+                    status_code=200,
+                    status_message="OK",
+                    data=output,
+                    document=is_document
+                )
+            else:
+                # format the response as not found 
+                [header, data] = formatResponse(
+                    client_address=client_address,
+                    method=method,
+                    route=route,
+                    status_code=404,
+                    status_message="Not Found",
+                    data="Not Found"
+                )
+            
+            # serve the response header
+            client_socket.sendall(header)
+            # serve the response data in chunks of buffer size
+            for i in range(0, len(data), BUFFER_SIZE):
+                # extract the required chunk size
+                chunk = data[i:i + BUFFER_SIZE]
+                # send chunk
+                client_socket.sendall(chunk)
+        
+    # close client socket connection   
+    client_socket.close()
 
 def init():
     global server
@@ -112,83 +189,26 @@ def init():
     # keep the server active
     while True:
         client_socket, client_address = server.accept()
-        [method, path] = formatRequest(client_socket)
-
-        client_socket.settimeout(30)
-
-        # if method or path is not found continue to the next
-        if not method or not path:
-            continue
         
-        # if the method is get::proceed
-        if method == 'GET':
-            path = formatPath(path)
-            
-            if len(glob.glob(path)) > 0:
-                # set the found file path
-                path = glob.glob(path)[0]
-                # extract the file extension
-                extension = getExtension(path)
-                # get the content type and execution type
-                content_type, execution_type = content_types.get(extension, ["text/plain", None])
-                # set is document to False
-                is_document = False
-                
-                if execution_type:
-                    # execute the file (eg: php) and get the output
-                    output = subprocess.check_output([execution_type, path], shell=True, universal_newlines=True)
-                elif "image" in content_type:
-                    # read the file as binary
-                    with open(path, 'rb') as file:
-                        output = file.read()
-                    # set is document True to format the response
-                    is_document = True
-                else:
-                    # read the file as text
-                    with open(path, 'r') as text:
-                        output = text.read()
-            
-                # format the response with data
-                [header, data] = formatResponse(
-                    method=method,
-                    path=path,
-                    content_type=content_type,
-                    status_code=200,
-                    status_message="OK",
-                    data=output,
-                    document=is_document
-                )
-            else:
-                # format the response as not found 
-                [header, data] = formatResponse(
-                    method=method,
-                    path=path,
-                    status_code=404,
-                    status_message="Not Found",
-                    data="Not Found"
-                )
-            
-            # serve the response header
-            client_socket.sendall(header)
-            # serve the response data in chunks of buffer size
-            for i in range(0, len(data), BUFFER_SIZE):
-                # extract the required chunk size
-                chunk = data[i:i + BUFFER_SIZE]
-                # send chunk
-                client_socket.sendall(chunk)
-        
-        time.sleep(0.1)
-        # close client socket connection   
-        client_socket.close()
+        # create the thread
+        thread = Thread(target=on_client, args=(client_socket, client_address))
+        # daemon is true to make sure all processes stop when program exits
+        thread.daemon = True
+        # start the thread
+        thread.start()  
             
 # __name__ is used to make sure the server is executed directly
 # not imported as a module
 if __name__ == '__main__':
     try:
+        # create folder if does not exist
+        if(not os.path.exists(FILE_DIR)):
+            os.mkdir(FILE_DIR)
+
         init()
-    except KeyboardInterrupt:
-        # handle Ctrl+C (KeyboardInterrupt)
-        sys.exit(0)
+    except Exception as e:
+        # print error
+        print(str(e))
     finally:
         # either way close the socket server
         server.close()
